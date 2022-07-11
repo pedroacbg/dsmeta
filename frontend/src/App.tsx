ae53092ea182063ed5d77b3d9b08b35dcec330b5
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2022-07-11 14:21:47 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import NotificationButton from "./components/NotificationButton";
+
 function App() {
-  return(
-    <h1>SALVE</h1>
-  )
+  return (
+    <>
+      <h1>SALVE</h1>
+      <NotificationButton />
+    </>
+  );
 }
 
-export default App
+export default App;
